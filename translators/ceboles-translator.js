708a2deb34bbd744eadfb6c332930166bd94d251
Joel Machado Junior <machado8junior@gmail.com>
2022-08-05 21:56:56 -0300

    first commit

---
+++ translators/ceboles-translator.js
@@ -0,0 +1,35 @@
+const btn_enviarTexto = document.querySelector("#btn-send-text");
+const textoEnviado = document.querySelector("#text-original");
+const textoTraduzido = document.querySelector("#text-translated");
+let novoTexto = [];
+
+btn_enviarTexto.addEventListener('click', () => {
+    novoTexto = [];
+    separaPalvras(textoEnviado.value);
+});
+
+function separaPalvras(palavras) {
+    const palavrasSeparadas = palavras.split(" ");
+    
+    for (el of palavrasSeparadas) {
+        novoTexto.push(trocaLetras(el));
+    }
+    textoTraduzido.value = novoTexto.join(" ");
+}
+
+function trocaLetras(palavra) {
+    let plvr;
+    let novaPalavra = palavra.replaceAll('rr', 'l');
+    
+    if (novaPalavra.includes('R') || novaPalavra.includes('r')) {
+        for(letra of novaPalavra) {
+            if(letra === 'R' && novaPalavra.indexOf('R') !== (novaPalavra.length - 1) ||
+            letra === 'r' && novaPalavra.indexOf('r') !== (novaPalavra.length - 1)) {
+                plvr = novaPalavra.replace('R', 'L').replace('r', 'l');
+                novaPalavra = plvr;
+            }
+        }
+    } 
+
+    return novaPalavra;
+}
