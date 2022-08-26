f4c55c0a6cf99d553d0b87c03f8699c4283b3b4e
Joel Machado Junior <machado8junior@gmail.com>
2022-08-26 02:09:55 -0300

    add onresize in eyes-movement.js

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 Tradutor para o Cebolês, a língua do personagem Cebolinha. Os conhecedores do personagem sabem que ele troca a letra 'r' pela letra 'l'. O que alguns não sabem é que existe uma exceção para essa regra: ele não troca as letras se o 'r' for a última letra da palavra. 
 
 Próximos passos:
-> Ajustar o código de responsividade, pensando na altura das telas.
-> Verificar quando o widthWindow mudar no eyes-moviment.js.
 > Refatorar as regras do translator.
+> Colocar botão X para apagar todo o texto digitado.
 > Adicionar botões para versões de texto, para Whatsapp e HTML, nas quais o R trocado seja negritado.
 > Adicionar o restante do conteúdo.
-> Colocar botão X para apagar todo o texto digitado.
 > Considerar colocar o div do olhos no formato oval, para melhorar o posionamento dos olhos pretos.
+> No eyes-moviment.js, retornar o black-eyes ao posicionamento original quando não tiver mais o movimento.
+> No eyes-moviment.js, pensar numa forma mais elegante de resolver o onmousemove quando não tiver mais o movimento dos olhos
--- scripts/eyes-moviment.js
+++ scripts/eyes-moviment.js
@@ -1,16 +1,26 @@
 const blackEyes = document.querySelector("#eyes-black");
-const widthWindow = window.innerWidth;
+let widthWindow = window.innerWidth;
 
 
-console.log(widthWindow);
+moviment();
 
-if(widthWindow > 860) {
-    document.onmousemove = function(e) {
-        var x = e.clientX * 100 / window.innerWidth + "%";
-        var y = e.clientY * 100 / window.innerHeight + "%";
-        
-        blackEyes.style.left = x; 
-        blackEyes.style.top = y;  
-        blackEyes.style.transform = `translate(-${x}, -${y})`;
+function moviment() {
+    widthWindow = window.innerWidth;
+
+    if(widthWindow > 860) {
+        document.onmousemove = function(e) {
+            var x = e.clientX * 100 / window.innerWidth + "%";
+            var y = e.clientY * 100 / window.innerHeight + "%";
+            
+            blackEyes.style.left = x; 
+            blackEyes.style.top = y;  
+            blackEyes.style.transform = `translate(-${x}, -${y})`;
+        }
+    } else {
+        document.onmousemove = function() {
+
+        }
     }
-}
+};
+
+window.onresize = moviment;
