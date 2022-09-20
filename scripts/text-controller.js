a731deb1a274470c1724c978ac55410866b01cd6
Joel Machado Junior <machado8junior@gmail.com>
2022-09-20 01:22:17 -0300

    create code to actived buttons languages in text-controller.js

--- scripts/text-controller.js
+++ scripts/text-controller.js
@@ -1,7 +1,8 @@
-const verifyText = document.querySelector('#text-original');
 const btnDeleteText = document.querySelector('#btn-delete-text');
 const sendedText = document.querySelector('#text-original');
 const translatedText = document.querySelector('#text-translated');
+const languages = document.getElementsByClassName('language');
+let indexBtnLang;
 
 
 btnDeleteText.addEventListener('click', () => {
@@ -10,10 +11,23 @@ btnDeleteText.addEventListener('click', () => {
     btnDeleteText.style.display = 'none';
 });
 
-verifyText.addEventListener('input', () => {
-    if (verifyText.value === "") {
+sendedText.addEventListener('input', () => {
+    if (sendedText.value === "") {
         btnDeleteText.style.display = 'none';
     } else {
         btnDeleteText.style.display = 'flex';
     }
 });
+
+for(let i = 0; i < languages.length; i++) {
+    languages[i].addEventListener('click', () => {
+        buttonLanguage(i);
+    });
+}
+
+function buttonLanguage(value) {
+    for(let y = 0; y < languages.length; y++) {
+        languages[y].classList.remove('active');
+    }
+    languages[value].classList.add('active');
+}
